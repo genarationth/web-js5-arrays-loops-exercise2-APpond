65b6694b510b17a8780fc0da3056b5b2fbd17857
ApPond <you@example.com>
2024-05-15 15:03:50 +0700

    finished

---
+++ script.js
@@ -0,0 +1,12 @@
+// Part 1: 
+let queue = ['Sofia', 'David', 'Juan'];
+console.log(queue);
+// Part 2: 
+queue.push('Sara', 'Augustin');
+console.log(queue);
+queue.shift();
+console.log(queue);
+// Part 3: 
+queue.splice(1, 0, 'Renata'); 
+queue.push('Elena');
+console.log(queue);
